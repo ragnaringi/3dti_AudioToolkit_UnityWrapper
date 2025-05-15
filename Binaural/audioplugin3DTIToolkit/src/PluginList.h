9218c07a09ed1cc0a23659a76b1a62962e9fd0b2
Ragnar Hrafnkelsson <ragnar.hrafnkelsson@gmail.com>
2025-05-15 12:23:18 +0100

    Rename 3DTI Binaural Spatializer > BRT Binaural Spatialiser

--- Binaural/audioplugin3DTIToolkit/src/PluginList.h
+++ Binaural/audioplugin3DTIToolkit/src/PluginList.h
@@ -1,2 +1,2 @@
-DECLARE_EFFECT("3DTI Binaural Spatializer", SpatializerSource3DTI)
+DECLARE_EFFECT("BRT Binaural Spatialiser", SpatializerSource3DTI)
 DECLARE_EFFECT("BRT Manager", SpatialiserCore3DTI)
